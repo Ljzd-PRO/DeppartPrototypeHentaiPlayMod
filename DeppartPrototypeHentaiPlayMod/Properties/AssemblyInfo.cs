9005f08ad3a9204e2792bfc3f8b8852116189284
Ljzd-PRO <63289359+Ljzd-PRO@users.noreply.github.com>
2024-05-11 23:07:40 +0800

    build: update AssemblyInfo.cs

--- DeppartPrototypeHentaiPlayMod/Properties/AssemblyInfo.cs
+++ DeppartPrototypeHentaiPlayMod/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DeppartPrototypeHentaiPlayMod")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Buttplug.io game mod for DeppartPrototype | 适配郊狼 3.0 DG-Lab-V3")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ljzd-PRO")]
 [assembly: AssemblyProduct("DeppartPrototypeHentaiPlayMod")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © 2024, Ljzd-PRO")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
